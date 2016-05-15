4b6c21a10f49c6f9251a2a27ad98e3d0581544e0
ina-rudovich <s120705@yandex.by>
2016-05-15 12:55:01 +0300

    saving tree

--- ArchiveProgram/ArchiveProgram/ArchiveProgram.cpp
+++ ArchiveProgram/ArchiveProgram/ArchiveProgram.cpp
@@ -10,12 +10,12 @@
 #include <vector>
 #include <map>
 #include <list>
-
+#include <ctime>
 
 using namespace std;
 
 typedef unsigned char uchar_byte;
-typedef char uchar;
+typedef unsigned char uchar;
 
 class Node
 {
@@ -23,12 +23,14 @@ public:
 	int weight;
 	uchar byte;
 	Node *left, *right;
+	bool sheet;
 
 	Node(Node *Left, Node *Right)
 	{
 		left = Left;
 		right = Right;
 		weight = Left->weight + Right->weight;
+		sheet = false;
 	}
 	Node() {}
 };
@@ -37,7 +39,7 @@ struct Compare
 {
 	bool operator()(Node *left, Node *right) const
 	{
-		return left->weight < right->weight;
+		return left->weight <= right->weight;
 	}
 };
 
@@ -45,34 +47,42 @@ streamoff file_size(fstream &myfile);
 fstream open_file(void);
 vector<uchar> read_file(fstream &myfile);
 void write_file(vector<uchar> & buffer);
-map<uchar, int> frequency_of_occurrences(vector<uchar> buffer);
-Node* build_tree(map<uchar, int> rates, Node* root);
+map<uchar, int> frequency_of_occurrences(vector<uchar> &buffer);
+Node* build_tree(map<uchar, int> &rates, Node* root);
 void print_tree(Node *root);
 void Association_code_byte(Node* root, map<uchar, vector<bool> > &table, vector <bool> &code);
-vector<bool> encryption(map<uchar, vector<bool> > table, vector<uchar> buffer);
+vector<bool> encryption(map<uchar, vector<bool> > &table, vector<uchar> &buffer);
 vector<uchar_byte> compiling_an_array_of_bytes(vector<bool> bin_mass);
 vector<bool> decryption(vector<uchar_byte> byte_mass);
 vector<uchar> byte_mass_recovery(Node* root, vector<bool> bin_mass);
+void add_base_info_for_decode(map<uchar, vector<bool> > table, fstream &myfile, string str);
+void get_base_info_for_decode(fstream & myfile, map<uchar, vector<bool> > &table, string &file_name);
+Node* make_tree_for_decode(map<uchar, vector<bool> > &table);
+
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	fstream myfile;
-
 	//myfile = open_file();
-	/*myfile.open("D:\\ЛАБЫ\\ArchiveProgram\\ArchiveProgram\\ArchiveProgram\\test.txt", ios::binary | ios::in);
+	myfile.open("..//..//..//tests//cube.obj", ios::binary | ios::in);
 	vector <uchar> buffer;
 	buffer = read_file(myfile);
-	myfile.close();*/
+	myfile.close();
+	cout << buffer.size() << endl;
+	cout << "clock1 --- " << clock()/ CLK_TCK  << endl;
+
+	/*for (int i = 0; i < buffer.size(); i++)
+		cout << (int)buffer[i] << " ";*/
 	
-	vector<uchar> buffer;
-	string str = "it is string for tests!!!!!!!";
+	/*vector<uchar> buffer;
+	string str = "it is my stringggggg!!!!!!!";
 
 	for (unsigned int i = 0; i < str.size(); i++)
 	{
 		buffer.push_back(str[i]);
-	}
+	}*/
 
 	map<uchar, int> rates = frequency_of_occurrences(buffer);
 
@@ -83,23 +93,31 @@ int main()
 	vector<bool> code;
 	map < uchar, vector<bool> >table;
 	Association_code_byte(root, table, code);
+	
+	fstream huff_file("..//..//..//tests//tree.hfmn", ios::binary | ios::in | ios :: app);
+	add_base_info_for_decode(table, huff_file, "test.txt");
+	
+	map < uchar, vector<bool> > t;
+	string name;
+	get_base_info_for_decode(huff_file, t, name);
+	huff_file.close();
+	Node *tree = make_tree_for_decode(t);
+	//print_tree(tree);
 
 	vector<bool> bin_mass = encryption(table, buffer);
 
 	vector<uchar_byte> new_byte_mass = compiling_an_array_of_bytes(bin_mass);
 
+	write_file(new_byte_mass);
+	cout << "clock7 --- " << clock()  << endl;
+
 	vector<bool> bin_mass_from_decryption = decryption(new_byte_mass);
 
-	vector<uchar> decipher_mass = byte_mass_recovery(root, bin_mass_from_decryption);
+	vector<uchar> decipher_mass = byte_mass_recovery(tree, bin_mass_from_decryption);
 
-	for (size_t i = 0; i < decipher_mass.size(); i++)
-	{
-		cout << decipher_mass[i];
-	}
-	cout << endl;
-
-	//write_file(buffer);
+	write_file(decipher_mass);
 	//system("cls");
+	cout << "clock --- " << clock()/ CLK_TCK << endl;;
 	system("pause");
     return 0;
 }
@@ -153,9 +171,9 @@ vector<uchar> read_file(fstream &myfile)
 void write_file(vector<uchar> & buffer)
 {
 	string str;
-	/*cout << "Введите имя для сжатого файла" << endl;
-	cin >> str;*/
-	str = "Compressed_Huffman.hfmn";
+	cout << "Введите имя для сжатого файла" << endl;
+	cin >> str;
+	//str = "Compressed_Huffman.hfmn";
 	fstream myfile(str, ios::binary | ios::out);
 
 	for (size_t i = 0; i < buffer.size(); i++) {
@@ -167,7 +185,7 @@ void write_file(vector<uchar> & buffer)
 }
 
 /*Подсчет частот*/
-map<uchar, int> frequency_of_occurrences(vector<uchar> buffer)
+map<uchar, int> frequency_of_occurrences(vector<uchar> &buffer)
 {
 	map<uchar, int> rates;
 
@@ -179,7 +197,7 @@ map<uchar, int> frequency_of_occurrences(vector<uchar> buffer)
 }
 
 /*Построение дерева*/
-Node* build_tree(map<uchar, int> rates, Node* root)
+Node* build_tree(map<uchar, int> &rates, Node* root)
 {
 	list <Node*> nodes;
 
@@ -191,6 +209,7 @@ Node* build_tree(map<uchar, int> rates, Node* root)
 		p->weight = i->second;
 		p->left = NULL;
 		p->right = NULL;
+		p->sheet = true;
 		nodes.push_back(p);
 	}
 
@@ -227,7 +246,7 @@ void Association_code_byte(Node* root, map<uchar, vector<bool> > &table, vector 
 		Association_code_byte(root->right, table, code);
 		code.pop_back();
 	}
-	if (root->byte)
+	if (root->sheet)
 	{
 		table[root->byte] = code;		
 	}
@@ -238,10 +257,10 @@ void print_tree(Node *root)
 {
 	if (root != NULL)
 	{
-		if(root->byte)
-		cout << "weight - " << root->weight << " : byte - "<< root->byte << endl;
+		if(root->sheet)
+		cout <<  " : byte - "<< root->byte << endl;
 		else
-			cout << "weight - " << root->weight << endl;
+			cout << "***" << endl;
 
 		print_tree(root->left);
 		print_tree(root->right);
@@ -250,7 +269,7 @@ void print_tree(Node *root)
 }
 
 /*получение булевого массива для сжатия*/
-vector<bool> encryption(map<uchar, vector<bool> > table, vector<uchar> buffer)
+vector<bool> encryption(map<uchar, vector<bool> > &table, vector<uchar> &buffer)
 {
 	vector<bool> bin_mass;
 	for (size_t i = 0; i < buffer.size(); i++)
@@ -268,32 +287,32 @@ vector<uchar_byte> compiling_an_array_of_bytes(vector<bool> bin_mass)
 	vector<uchar_byte> new_byte_mass;
 	new_byte_mass.push_back(0);
 
-	while ((bin_mass.size() / 8))
+	size_t k = 0;
+	size_t mass_bytes = bin_mass.size() - bin_mass.size() % 8;
+	for (k; k < mass_bytes;)
 	{
 		uchar_byte temp_byte = 0;
 		for (int i = 7; i >= 0; i--)
 		{
-			temp_byte += bin_mass.front()*((uchar_byte) pow(2, i));
-			bin_mass.erase(bin_mass.begin(), bin_mass.begin()+1);
+			temp_byte += bin_mass[k]*((uchar_byte) pow(2, i));
+			k++;
 		}
 		new_byte_mass.push_back(temp_byte);
+		
 	}
-
-	bool t = bin_mass.empty();
+	cout << "new_byte_mass --- "<<new_byte_mass.size() << endl;
+	bool t = (k == bin_mass.size());
 		if (!t)
 		{
-			new_byte_mass[0] = (uchar_byte)bin_mass.size();
+			new_byte_mass[0] = (uchar_byte)bin_mass.size() % 8;
 			uchar_byte temp_byte = 0;
-			uchar_byte i = 0;
-			while (bin_mass.size() != 0)
+			for (uchar_byte i = new_byte_mass[0]; i >0 ;i--)
 			{
-				temp_byte += bin_mass.back() * ((uchar_byte) pow(2, i));
-				bin_mass.pop_back();
-				i++;
+				temp_byte += bin_mass[k] * ((uchar_byte) pow(2, i-1));
+				k++;			
 			}
 			new_byte_mass.push_back(temp_byte);
 		}
-
 	return new_byte_mass;
 }
 
@@ -374,7 +393,7 @@ vector<uchar> byte_mass_recovery(Node* tree_root, vector<bool> bin_mass)
 	while (bin_mass.size() != 0)
 	{
 		Node* root = tree_root;
-		while (!(root->byte))
+		while (!(root->sheet))
 		{
 			if (bin_mass.front() == 0)
 				root = root->left;
@@ -384,5 +403,121 @@ vector<uchar> byte_mass_recovery(Node* tree_root, vector<bool> bin_mass)
 		}
 		byte_mass.push_back(root->byte);
 	}
+	cout << "byte_mass --- " << byte_mass.size() << endl;
 	return byte_mass;
+}
+
+/*Сохранение "z" + имени в файл */
+void add_base_info_for_decode(map<uchar, vector<bool> > table, fstream &myfile,string str)
+{
+	myfile << (uchar) str.size();
+
+	for (int i = 0; i < str.size(); i++)
+		myfile << (uchar)str[i];
+	cout << "table size in add... " << table.size() << endl;
+	myfile << (uchar)(table.size()-1);
+	cout << "file-size" <<file_size(myfile) << endl;
+
+	map<uchar, short> compressed_tree;
+
+	for (map<uchar, vector<bool> >::iterator i = table.begin(); i != table.end(); i++)
+	{
+		uchar k = (i->second).size();
+		short temp = (short)pow(2, k);
+
+		for (size_t j = 0; j <k; j++)
+		{
+			temp += (i->second)[j]*((short)pow(2, (k-j-1)));			
+		}
+		compressed_tree[i->first] = temp;
+	}
+
+	for (map<uchar, short> ::iterator i = compressed_tree.begin(); i != compressed_tree.end(); i++)
+	{
+		myfile.write((char*)&(i->first),1);
+		myfile.write((char*)&(i->second), 2);
+	}
+	cout << "file_size === " <<file_size(myfile) << endl;
+	
+}
+
+/*Получение имени и информации для восстановления*/
+void get_base_info_for_decode(fstream & myfile, map<uchar, vector<bool> > &table, string &file_name)
+{
+	cout << "get_base_info_for_decode" << endl;
+	cout << "file_size === " << file_size(myfile) << endl;
+	uchar str_length ;
+	myfile.read((char*)&str_length, 1);
+	cout << "str_len --- " << (int)str_length << endl;
+	file_name.resize(str_length);
+	for (int i = 0; i < str_length; i++)
+	{
+		file_name[i] = myfile.get();
+	}
+
+	cout << file_name << endl;
+
+	uchar table_size;
+	myfile.read((char*)&table_size, 1);
+	cout << "table_size" <<(int) table_size << endl;
+
+	map<uchar, short> compressed_tree;
+	for (short i = 0; i <= table_size; i++)
+	{
+		uchar temp_byte;
+		short temp_value;
+		myfile.read((char*)&temp_byte, 1);
+		myfile.read((char*)&temp_value, 2);
+		compressed_tree[temp_byte] = temp_value;
+	}
+
+	for (map<uchar, short> ::iterator i = compressed_tree.begin(); i != compressed_tree.end(); i++)
+	{
+		vector<bool> code;
+		while (i->second != 1)
+		{
+			bool temp = ((i->second % 2) != 0);
+			code.push_back(temp);
+			i->second /= 2;
+		}
+
+		for (int j = code.size() - 1; j>= 0; j--)
+		{
+			table[i->first].push_back(code[j]);
+		}
+	}
+}
+
+/*Восстановление дерева*/
+Node* make_tree_for_decode(map<uchar, vector<bool> > &table)
+{
+	Node* tree =new Node ;
+	for (map<uchar, vector<bool> >::iterator i = table.begin(); i != table.end(); i++)
+	{
+		Node *root = tree;
+		for (size_t j = 0; j < i->second.size(); j++)
+		{
+			if (i->second[j] == 0)
+			{
+				if (root->left == NULL)
+				{
+					Node *r = new Node;
+					root->left = r;
+				}
+				root = root->left;
+			}
+			else
+			{
+				if (root->right == NULL)
+				{
+					Node *r = new Node;
+					root->right = r;
+				}
+				root = root->right;
+			}
+		}
+		root->byte = i->first;
+		root->sheet = true;
+	}
+	return tree;
 }
